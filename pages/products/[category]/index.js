c25b4e18916163ac9684094fbad9da64c8d5e2f6
Basit Minhas <basit3407@gmail.com>
2021-03-10 15:17:52 +0500

    may done maybe not

--- pages/products/[category]/index.js
+++ pages/products/[category]/index.js
@@ -241,8 +241,9 @@ MapArray.propTypes = {
 export async function getServerSideProps(context) {
   const { category } = context.params;
   try {
-    const { data } = await axios.get(`https://saqee-onlinestore.vercel.app
-/api/products/?category=${category}`);
+    const { data } = await axios.get(
+      `https://saqee-onlinestore.vercel.app/api/products/?category=${category}`
+    );
     return {
       props: {
         array: data.products,
