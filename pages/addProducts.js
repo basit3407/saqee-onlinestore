3d8c80cdb71c957cbf10fd559fc39bd03d07da0e
Basit Minhas <basit3407@gmail.com>
2021-03-26 12:47:07 +0500

    api address changed to vercel app domain

--- pages/addProducts.js
+++ pages/addProducts.js
@@ -176,7 +176,7 @@ export default function AddProducts() {
   //This function handles click on dispatch button
   const handleClick = () =>
     axios
-      .post("http://localhost:3000/api/products", product)
+      .post("https://saqee-onlinestore.vercel.app/api/products", product)
       .then(() => {
         setError({});
         setSuccess(true);
