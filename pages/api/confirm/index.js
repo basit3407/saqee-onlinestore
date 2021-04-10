2bb83eb0eda84db25cc927afd39a33a2f72b9a01
Basit Minhas <basit3407@gmail.com>
2021-04-10 14:57:33 +0500

    testing on vercel

--- pages/api/confirm/index.js
+++ pages/api/confirm/index.js
@@ -7,6 +7,8 @@ const handler = nc().post(async (req, res) => {
   const { name, email } = req.body;
   const { db } = await connectToDatabase();
 
+  console.log("i got called");
+
   const user = await db.collection("users").findOne({ email });
   if (!user) return res.status(400).json({ error: "user doesnt exist" });
   if (user.isVerified)
