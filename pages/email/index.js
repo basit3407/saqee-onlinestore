1e5ee2eaa74a222eba59e472fd9f887462516156
Basit Minhas <basit3407@gmail.com>
2021-04-11 13:58:18 +0500

    again adjusted padding

--- pages/email/index.js
+++ pages/email/index.js
@@ -63,7 +63,7 @@ export default function Email() {
 
   return (
     <Box
-      p={matches ? 10 : 20}
+      p={matches ? 15 : 40}
       display="flex"
       flexDirection="column"
       justifyContent="center"
